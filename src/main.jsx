571cd28f4c17abd11b0f800e85cf5271d09a0638
killup10 <juanjose.gold10@gmail.com>
2025-05-02 19:47:36 -0500

    Fix main.jsx to properly mount React app

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,43 @@
 // src/main.jsx
-// Este archivo arranca tu aplicación React Router + IPTVApp
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './utils/AuthContext.jsx';
-import { PrivateRoute } from './utils/PrivateRoute.jsx';
-import IPTVApp from './IPTVApp.jsx';
+import PrivateRoute from './components/PrivateRoute.jsx';
+import AdminRoute from './components/AdminRoute.jsx';
+
 import { Login } from './pages/Login.jsx';
 import { Register } from './pages/Register.jsx';
-
+import Catalog from './pages/Catalog.jsx';
+import Player from './pages/Player.jsx';
+import IPTVApp from './pages/IPTVApp.jsx';
+import AdminPanel from './pages/AdminPanel.jsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          {/* Rutas públicas */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-          {/* Ruta principal IPTV, protegida */}
-          <Route
-            path="/*"
-            element={
-              <PrivateRoute>
-                <IPTVApp />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
-  </React.StrictMode>
+root.render(
+  <AuthProvider>
+    <Router>
+      <Routes>
+        {/* Rutas públicas */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Rutas protegidas */}
+        <Route path="/" element={<PrivateRoute><Catalog /></PrivateRoute>} />
+        <Route path="/movies" element={<PrivateRoute><Catalog type="movie"/></PrivateRoute>} />
+        <Route path="/series" element={<PrivateRoute><Catalog type="series"/></PrivateRoute>} />
+        <Route path="/player/:id" element={<PrivateRoute><Player/></PrivateRoute>} />
+        <Route path="/iptv" element={<PrivateRoute><IPTVApp/></PrivateRoute>} />
+
+        {/* Ruta /admin solo para admins */}
+        <Route path="/admin" element={<AdminRoute><AdminPanel/></AdminRoute>} />
+
+        {/* Fallback */}
+        <Route path="*" element={<PrivateRoute><Catalog/></PrivateRoute>} />
+      </Routes>
+    </Router>
+  </AuthProvider>
 );
