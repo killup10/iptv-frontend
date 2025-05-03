a00684da3ba1af094a9490631c62c50e1f12eb79
killup10 <juanjose.gold10@gmail.com>
2025-05-03 02:00:13 -0500

    fix: corregir import de IPTVApp desde src/ en main.jsx y App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { Login } from "./pages/Login.jsx";
 import { Register } from "./pages/Register.jsx";
 import Catalogo from "./pages/Catalogo.jsx";
 import Player from "./pages/Player.jsx";
-import IPTVApp from "./pages/IPTVApp.jsx";
+import IPTVApp      from './IPTVApp.jsx';
 import AdminPanel from "./pages/AdminPanel.jsx";
 
 function AppContent() {
--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { Login } from './pages/Login.jsx';
 import { Register } from './pages/Register.jsx';
 import Catalogo from './pages/Catalogo.jsx';
 import Player from './pages/Player.jsx';
-import IPTVApp from './pages/IPTVApp.jsx';
+import IPTVApp      from './IPTVApp.jsx';
 import AdminPanel from './pages/AdminPanel.jsx';
 
 import './index.css';
