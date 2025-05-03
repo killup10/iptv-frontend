1087d6d7e159b374769d72f686b31b9db2351901
killup10 <juanjose.gold10@gmail.com>
2025-05-03 02:44:04 -0500

    Fix: corrige imports y export de Player como componente nombrado

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import AdminRoute from './components/AdminRoute.jsx';
 import { Login } from './pages/Login.jsx';
 import { Register } from './pages/Register.jsx';
 import { Catalogo } from './pages/Catalogo.jsx';
-import Player from './pages/Player.jsx';
+import { Player } from './pages/Player.jsx';
 import IPTVApp      from './IPTVApp.jsx';
 import AdminPanel from './pages/AdminPanel.jsx';
 
