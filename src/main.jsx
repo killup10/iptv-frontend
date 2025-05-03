01eb3c9505c884da96897e92f13f8ef9615fc052
killup10 <juanjose.gold10@gmail.com>
2025-05-03 02:26:10 -0500

    fix: importar PrivateRoute como named export

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './utils/AuthContext.jsx';
-import PrivateRoute from './utils/PrivateRoute.jsx';
+import { PrivateRoute } from './utils/PrivateRoute.jsx';
 import AdminRoute from './components/AdminRoute.jsx';
 
 import { Login } from './pages/Login.jsx';
