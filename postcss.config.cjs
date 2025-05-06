6abadf1036f2b154633da4f14f93c0909316feb8
killup10 <juanjose.gold10@gmail.com>
2025-05-06 09:39:02 -0500

    Arreglar configuración de PostCSS

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,6 @@
-$postcssContent = @'
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
-'@
-
-$postcssContent | Out-File -FilePath "postcss.config.cjs" -Encoding utf8
